fcda437ca7032fdc1044833748b287ea8237b179
Dmitry Marchenko <dmmarychev@yandex.ru>
2020-03-12 16:54:33 +0300

    Add solution for Task4 and part of Task 5

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -6,17 +6,66 @@
 - (NSArray <NSString*>*)parseString:(NSString*)string {
    
     NSMutableString *sourceString = [NSMutableString stringWithString:string];
-                         
+    NSMutableArray *resultArray = [NSMutableArray array];
+    
+    NSCharacterSet *openSet = [NSCharacterSet characterSetWithCharactersInString:@"[(<"];
+    
     for (int i = 0; i < [sourceString length]; i++) {
         
         NSString *currentCharacter = [sourceString substringWithRange:NSMakeRange(i, 1)];
         
-        NSLog(@"%@", currentCharacter);
-        
+        if ([currentCharacter rangeOfCharacterFromSet:openSet].location != NSNotFound) {
+            
+            NSMutableString *resultString = [NSMutableString string];
+            int repeatCounter = 0;
+            
+            for (int j = i + 1; j < [sourceString length]; j++) {
+                
+                NSString *resultCharacter = [sourceString substringWithRange:NSMakeRange(j, 1)];
+                
+                if ([resultCharacter isEqualToString:[self closeBracketForOpenBracket:currentCharacter]] && repeatCounter == 0) {
+                    
+                    [resultArray addObject:resultString];
+                    break;
+                    
+                }  else if ([resultCharacter isEqualToString:[self closeBracketForOpenBracket:currentCharacter]] && repeatCounter != 0){
+                    
+                    [resultString appendString:resultCharacter];
+                    repeatCounter--;
+                    
+                } else if ([resultCharacter isEqualToString:currentCharacter]) {
+                    
+                    repeatCounter++;
+                    [resultString appendString:resultCharacter];
+                    
+                } else {
+                    [resultString appendString:resultCharacter];
+                }
+            }
+        }
     }
-                         
     
-    return @[@"Test"];
+    return resultArray;
+}
+
+- (NSString *)closeBracketForOpenBracket:(NSString *)openBracket {
+    
+    NSString *openSquareBracket = @"[";
+    NSString *closeSquareBracket = @"]";
+    NSString *openRoundBracket = @"(";
+    NSString *closeRoundBracket = @")";
+    NSString *openMoreLessBracket = @"<";
+    NSString *closeMoreLessBracket = @">";
+    
+    if ([openBracket isEqualToString:openSquareBracket]) {
+        return closeSquareBracket;
+    } else if ([openBracket isEqualToString:openRoundBracket]) {
+        return closeRoundBracket;
+    } else if ([openBracket isEqualToString:openMoreLessBracket]) {
+        return closeMoreLessBracket;
+    } else {
+        return @"";
+    }
 }
 
 @end
--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -4,7 +4,94 @@
 
 // Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
-    return @"";
+    
+    NSMutableString *resultNumberString = [NSMutableString stringWithString:s];
+    int numberOfDigitsInNumber = [n intValue];
+    int numberOfAvailableReplacements = [k intValue];
+    NSString *strinfForReplace = @"9";
+    
+    BOOL isNumberOfDigitsOdd = (numberOfDigitsInNumber % 2 != 0) ? YES : NO;
+    int numberOfDigitsToCheck = isNumberOfDigitsOdd ? (numberOfDigitsInNumber - 1 / 2) : (numberOfDigitsInNumber / 2);
+    int coincidencesCounter = 0;
+    
+    for (int i = 0; i < numberOfDigitsToCheck; i++) {
+        
+        NSString *firstNumber = [s substringWithRange:NSMakeRange(i, 1)];
+        NSString *secondNumber = [s substringWithRange:NSMakeRange(numberOfDigitsInNumber - (i + 1) , 1)];
+        
+        if ([firstNumber isEqualToString:secondNumber]) {
+            coincidencesCounter++;
+        }
+    }
+    
+    int numberOfMinReplacements = numberOfDigitsToCheck - coincidencesCounter;
+    
+    if (numberOfAvailableReplacements < numberOfMinReplacements) {
+        return @"-1";
+    } else if (numberOfAvailableReplacements == numberOfMinReplacements) {
+        
+        for (int i = 0; i < numberOfDigitsToCheck; i++) {
+            
+            NSRange rangeOfFirstNumber = NSMakeRange(i, 1);
+            NSRange rangeOfSecondNumber = NSMakeRange(numberOfDigitsInNumber - (i + 1) , 1);
+            
+            NSString *firstNumber = [s substringWithRange:rangeOfFirstNumber];
+            NSString *secondNumber = [s substringWithRange:rangeOfSecondNumber];
+            
+            if ([firstNumber isEqualToString:secondNumber]) {
+                continue;
+            } else {
+                if ([firstNumber intValue] > [secondNumber intValue]) {
+                    [resultNumberString setString:[resultNumberString stringByReplacingCharactersInRange:rangeOfSecondNumber withString:firstNumber]];
+                } else {
+                    [resultNumberString setString:[resultNumberString stringByReplacingCharactersInRange:rangeOfFirstNumber withString:secondNumber]];
+                }
+            }
+        }
+        return resultNumberString;
+    } else {
+        
+        for (int i = 0; i < numberOfDigitsToCheck; i++) {
+            
+            NSRange rangeOfFirstNumber = NSMakeRange(i, 1);
+            NSRange rangeOfSecondNumber = NSMakeRange(numberOfDigitsInNumber - (i + 1) , 1);
+            
+            NSString *firstNumber = [s substringWithRange:rangeOfFirstNumber];
+            NSString *secondNumber = [s substringWithRange:rangeOfSecondNumber];
+            
+            
+            if ([firstNumber isEqualToString:strinfForReplace] && numberOfAvailableReplacements != 0) {
+                [resultNumberString setString:[resultNumberString stringByReplacingCharactersInRange:rangeOfSecondNumber withString:strinfForReplace]];
+                numberOfAvailableReplacements--;
+            } else if ([secondNumber isEqualToString:strinfForReplace] && numberOfAvailableReplacements != 0) {
+                [resultNumberString setString:[resultNumberString stringByReplacingCharactersInRange:rangeOfFirstNumber withString:strinfForReplace]];
+                numberOfAvailableReplacements--;
+            } else {
+                continue;
+            }
+        }
+        
+        if (numberOfAvailableReplacements > 1) {
+            
+            
+            
+            return resultNumberString;
+            
+        } else if (numberOfAvailableReplacements == 1) {
+            
+            if (isNumberOfDigitsOdd) {
+                NSRange rangeOfMiddleDigit = NSMakeRange(numberOfDigitsToCheck + 1, 1);
+                [resultNumberString setString:[resultNumberString stringByReplacingCharactersInRange:rangeOfMiddleDigit withString:strinfForReplace]];
+            }
+            return resultNumberString;
+            
+        } else {
+            return resultNumberString;
+        }
+        
+        
+        
+    }
 }
 
 @end
